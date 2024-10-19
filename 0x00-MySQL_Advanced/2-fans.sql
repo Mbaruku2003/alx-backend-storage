18b03587f4bd582ec51ea2f5722382ff2f7f8567
Mbaruku2003 <leonardjembe21@gmail.com>
2024-10-20 00:40:43 +0300

    created a new file

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,3 +1,5 @@
 -- ranks countries in the order of non unique fans
-SELECT origin AS Origin, SUM(fans) AS SAM FROM metal_bands
-GROUP BY Origin ORDER BY SAM DESC;
+SELECT origin AS Origin, SUM(fans) AS total_fans
+FROM metal_bands
+GROUP BY Origin
+ORDER BY total_fans DESC;
