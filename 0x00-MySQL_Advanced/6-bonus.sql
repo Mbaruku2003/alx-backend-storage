14ec38bcb8d8e35077f4900665e997e39872127e
Mbaruku2003 <leonardjembe21@gmail.com>
2024-10-21 17:12:27 +0300

    created a new file

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -6,7 +6,7 @@ BEGIN
 		THEN
 		INSERT INTO projects(name) VALUES (project_name);
 	END IF;
-	INSERT INTO corrections (user_id, project_id, score)
-	VALUES (user_id, (SELECT id from projects WHERE name=project_name), score);
+	INSERT INTO corrections (score)
+	VALUES (score);
 END $$
 DELIMITER
