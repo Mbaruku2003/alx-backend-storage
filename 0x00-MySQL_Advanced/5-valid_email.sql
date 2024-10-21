28d6fd684de8c4febe36f21bb2ee0f02b3fd52f7
Mbaruku2003 <leonardjembe21@gmail.com>
2024-10-21 14:05:57 +0300

    created a new file

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,3 +1,8 @@
 -- resets the attribute valid_email only when the email has been changed.
 CREATE TRIGGER the_trigger AFTER UPDATE users
-FOR EACH ROW IN  users UPDATE valid_email
+FOR EACH ROW IN  users
+	BEGIN
+		IF NEW.email != OLD.email THEN
+			SET NEW.valid_email = 0;
+		END IF;
+	END
