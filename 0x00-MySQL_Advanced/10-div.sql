df02b34439bc2a10af37d8199c60d7b6bdec20c8
Mbaruku2003 <leonardjembe21@gmail.com>
2024-10-21 18:11:17 +0300

    created a new file

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,6 @@
+-- divides (and returns) the first by the second number or returns 0 if the second number is equal to 0.
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURN IF (b == 0)
+THEN RETURN 0
+ELSE
+	RETURN a / b
