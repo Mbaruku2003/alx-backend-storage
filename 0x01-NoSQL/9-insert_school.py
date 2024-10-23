d2c25687b9a87cf8f31d3e89fdbd77bb37648eaf
Mbaruku2003 <leonardjembe21@gmail.com>
2024-10-23 15:27:45 +0300

    created new file

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -4,5 +4,5 @@
 def insert_school(mongo_collection, **kwargs):
     """Returns the new _id."""
 
-    database = mongo_collection.insert_many(kwargs)
+    database = mongo_collection.insert_one(kwargs)
     return database.inserted_id
